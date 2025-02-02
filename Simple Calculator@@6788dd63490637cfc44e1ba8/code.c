23f91d4038733e043309d5bd9c9db0046d6a66d9
harjas1308 <harjas1308.becse24@chitkara.edu.in>
2025-02-02 12:42:39 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,15 +3,31 @@ int main(){
     float a,b;
     char sign;
     scanf("%f %f %c",&a,&b,&sign);
-    if(sign=='+'){
-    printf("%.0f",a+b);
-    }else if(sign=='-'){
+    // if(sign=='+'){
+    // printf("%.0f",a+b);
+    // }else if(sign=='-'){
+    //     printf("%.0f",a-b);
+    // }else if(sign=='*'){
+    //     printf("%.0f",a*b);
+    // }else if(sign=='/'){
+    //     printf("%.0f",a/b);
+    // }else{
+    //     printf("error");
+    // }
+    switch(sign){
+        case '+':
+        printf("%.0f",a+b);
+        break;
+        case '-':
         printf("%.0f",a-b);
-    }else if(sign=='*'){
+        break;
+        case '*':
         printf("%.0f",a*b);
-    }else if(sign=='/'){
+        break;
+        case '/':
         printf("%.0f",a/b);
-    }else{
+        break;
+        default:
         printf("error");
     }
     return 0;
