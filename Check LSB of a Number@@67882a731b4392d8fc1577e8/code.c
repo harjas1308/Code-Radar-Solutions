e0f031648a1b32a24cdd795a640303bdd29c485b
Harjas Kaur Tak <harjas2826@gmail.com>
2025-02-11 19:35:26 +0530

    Solution to Check LSB of a Number

--- Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -1,13 +1,18 @@
 // Your code here...
 #include <stdio.h>
 
-// Function to get the LSB of a number
-int get_lsb(int number) {
-    return number & 1;  // LSB can be found using bitwise AND with 1
+void check_lsb(int number) {
+    // Check if the LSB is set or not using bitwise AND with 1
+    if (number & 1) {
+        printf("The Least Significant Bit of %d is set (1).\n", number);
+    } else {
+        printf("The Least Significant Bit of %d is not set (0).\n", number);
+    }
 }
 
 int main() {
-    int number = 13;  // Example number (binary: 1101)
-    printf("The LSB of %d is: %d\n", number, get_lsb(number));
+    int number = 13; // Example number
+    check_lsb(number); // Check LSB of the example number
+    
     return 0;
 }
